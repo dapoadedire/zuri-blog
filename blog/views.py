a5cca61f5f634701a1aaae53851a185d30feab0f
dapoadedire <adedireadedapo19@gmail.com>
2022-06-22 21:05:10 +0100

    Last last

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from blog.models import Post
