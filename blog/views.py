3a3e0b7d450be7a73534ae0c3e7f77c2eb51594f
dapoadedire <adedireadedapo19@gmail.com>
2022-07-03 10:11:16 +0100

    refactor: format with black

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,13 @@ from django.views.generic.detail import DetailView
 
 from django.urls import reverse_lazy
 from blog.models import Post
+
 # Create your views here.
+
 
 class PostListView(ListView):
     model = Post
+
     def get_queryset(self):
         return super().get_queryset().filter(status="published")
 
@@ -18,18 +21,18 @@ class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
-  
+
+
 class PostDetailView(DetailView):
     model = Post
-   
+
+
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:all")
-   
 
 
 class PostDeleteView(DeleteView):
     model = Post
     success_url = reverse_lazy("blog:all")
-   
